d8bfb807ad01fe7327f35a750af0e85379b1cf6d
Ian <ianlloydwilkinson@gmail.com>
2020-11-15 15:37:52 -0700

    Added craps and hi low to github

---
+++ craps.rb
@@ -0,0 +1,74 @@
+require_relative "game.rb"
+require_relative "dice.rb"
+require_relative "wallet.rb"
+class Craps < Game
+
+##pass line bet
+##come-out roll
+  #if 7 or 11 pass bet wins
+  #if 2,3 or 12, dont pass wins
+  #if any other number, set that number to point bet
+    #continue to roll until 7, 2, 3, 11, or 12 is rolled
+
+  def start_game (total)
+    puts "You can win various amaounts in this game"
+    puts "What is your starting bet? Wallet total: #{total}"
+
+  end
+
+  def pass_bet 
+    puts "Will you bet pass line or don't pass line?"
+    puts "1.) Pass Line"
+    puts "2.) Don't Pass Line"
+    starting_bet = gets.chomp.to_i
+    return starting_bet 
+  end
+
+
+  def roll_result (dice, pass_choice)
+    case pass_choice
+    when 1
+      if (dice == 7 || dice == 11)
+        puts "You win!"
+      elsif (dice == 2 || dice == 3 || dice == 12)
+        puts "Roller craps out."
+        puts "You lose..."
+      else
+        puts "Second roll logic here"
+      end
+    when 2
+      if (dice == 2 || dice == 3 || dice == 12)
+        puts "You Win!"
+      elsif (dice == 7 || dice == 11)
+        puts "Roller point is established."
+        puts "You lose..."
+      else
+        puts "Second roll logic here"
+      end
+    end
+  end
+end
+
+@wallet = Wallet.new(100)
+@craps = Craps.new({
+  name: "Craps",
+  options: ["Play", "Check Balance", "Quit to menu"]
+})
+@d = Dice.new
+
+def craps_app
+  @craps.display_game
+  case @craps.get_selection
+  when 1
+    @craps.start_game(@wallet.total)
+    @wallet.bet_money
+    bet1 = @craps.pass_bet
+    @craps.roll_result(@d.show_sum, bet1)
+  when 2
+    @wallet.display_wallet
+  when 3
+    main_menu_app
+  end
+end
+
+craps_app
--- high_low.rb
+++ high_low.rb
@@ -1,2 +1,86 @@
-class High_low < Game
+require_relative "game.rb"
+require_relative "deck.rb"
+
+#play a card
+#Higher guess again
+#Lower guess again
+#correct answer Good Job!
+class Card
+  # Getter and Setter methods for rank, suit and color
+  attr_accessor :rank, :suit, :color
+  # Gets called when you call the new method to create an instance
+  # card = Card.new(‘10’, ‘K’, ‘Black’)
+  def initialize(rank, suit, color)
+    @rank = rank
+    @suit = suit
+    @color = color
+  end
 end
+class Highlow < Game
+  
+
+  def play 
+    first_card = @randomdeck.cards.sample.rank
+    puts "The first card is #{first_card}"
+    puts "Guess higher or lower"
+    puts "1 is lower, 2 is higher"
+    guess = gets.chomp.to_i
+    second_card = @randomdeck.cards.sample.rank
+    puts "The second card #{second_card}"
+    if (guess == 0)  
+      puts "integer"
+    else
+      (if (first_card < second_card)     
+        if (guess == 2)
+          puts "Card was higher, You win"
+        else (guess == 1)
+          puts "Card was lower, You lose"
+        end
+      elsif (first_card > second_card)
+        if (guess == 2)
+            puts "Card was higher, You lose"
+        else (guess == 1)
+            puts "Card was lower, You win"
+        end
+      else
+        puts "It was the same card, try again"
+      end)
+    end
+
+    
+
+
+
+
+  end
+end
+
+@deck = Deck.new
+high_low = Highlow.new({
+  name: "High/Low",
+  options: ["Play", "Check Balance", "Quit to menu"]
+})
+
+high_low.display_game
+highlow_menu_selection = high_low.get_selection
+puts highlow_menu_selection
+if ( highlow_menu_selection == 1)
+  high_low.play
+elsif ( highlow_menu_selection == 2)
+  #wallet value
+else
+  #quit to main menu
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
